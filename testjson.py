6a37bc7cfcdc99eb885817b938cf572dfdd3bb6c
progrramming <85759982+progrramming@users.noreply.github.com>
2021-06-11 20:34:17 +0300

    Add files via upload

---
+++ testjson.py
@@ -0,0 +1,19 @@
+import json
+
+# a Python object (dict):
+x = {
+  "name": "John",
+  "age": 30,
+  "city": "New York",
+  "test": "test2"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+
+with open("testwritejsonfile.json","a") as out:
+    #for i in y:
+      print(y,file=out)
+
